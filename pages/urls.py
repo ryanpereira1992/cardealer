b6eb84a04cb9c62728f3d761ccc626002f104637
Ryan Pereira <ryan_pereira_92@hotmail.com>
2021-10-12 10:30:51 +0530

    added pages and templates

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home()),
+    path('', views.home, name='home'),
+    path('cars', views.cars, name='cars'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
